72413be22a6757bed61a5b4c42245e46f52eaf3e
Cyril Rohr <cyril.rohr@gmail.com>
2017-07-24 22:06:12 +0200

    autoupgrade by default

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-if blog config:get CONFIGURED ; then
-	blog scale web=1
-	blog run pumactl -P tmp/pids/server.pid restart
+packager-blog scale web=1
+packager-blog run pumactl -P tmp/pids/server.pid restart
+
+if ! packager-blog config:get AUTOUPGRADE ; then
+	packager-blog config:set AUTOUPGRADE=true
 fi
